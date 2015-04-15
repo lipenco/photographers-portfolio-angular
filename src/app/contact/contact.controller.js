514c2453cadf727d427c30fb8df267296c0c289f
Magdalena Lipińska <mag.lipinska@gmail.com>
2015-04-15 19:09:07 +0900

    adds basic animations

--- src/app/contact/contact.controller.js
+++ src/app/contact/contact.controller.js
@@ -1,7 +1,10 @@
 'use strict';
 
 angular.module('kingaFrontend')
-  .controller('ContactCtrl', function ($scope) {
-
+  .controller('ContactCtrl', function ($scope, $timeout) {
+      $scope.showAnimation = false
+      $timeout(function() {
+        $scope.showAnimation = true
+      }, 400)
 
   });
