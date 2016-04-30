e055c15cdbfce1dc079b991642ab969af0b5b627
Magdalena Lipińska <mag.lipinska@gmail.com>
2016-04-30 21:19:09 +0100

    add loader

--- src/app/featured/featured.controller.js
+++ src/app/featured/featured.controller.js
@@ -2,11 +2,33 @@
 
 angular.module('kingaFrontend')
   .controller('FeaturedCtrl', function ($scope, kingaApi) {
+    $scope.photos = [
+      {
+        url: "./assets/images/1.jpg"
+      },
+      {
+        url: "./assets/images/3.jpg"
+      },
+      {
+        url: "./assets/images/4.jpg"
+      },
+      {
+        url: "./assets/images/5.jpg"
+      },
+      {
+        url: "./assets/images/6.jpg"
+      },
+    ]
+
+    setTimeout(function(){
+        $(".rslides").responsiveSlides();
+    }, 500)
+    $scope.loader = true;
 
     kingaApi.Photo.getFeaturedPhotos()
       .success(function (response) {
         $scope.photos = response
-
+        $scope.loader = false;
         setTimeout(function(){
             $(".rslides").responsiveSlides();
         })
